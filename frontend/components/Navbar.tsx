09ee5e7aac76191c46157118cd3a212e1f72fa1e
Ginika-Chinonso <ginika.chinonso.francis@gmail.com>
2023-10-17 22:58:35 +0100

    feat: Registration frontend template

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,4 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
@@ -17,6 +15,9 @@ const Navbar = () => {
             </li>
             <li>
                 <Link href={"/prescription"} >Prescription</Link>
+            </li>
+            <li>
+                <Link href={"/register"} >Register</Link>
             </li>
         </ul>
         <ConnectButton />
