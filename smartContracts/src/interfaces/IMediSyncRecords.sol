475711bbf5c83807d1b50be1ce44a4c9dfbe66a8
akinbola247 <akinbolakehinde247@gmail.com>
2023-10-19 02:10:26 +0100

    IMediSyncRecords.sol added

--- smartContracts/src/interfaces/IMediSyncRecords.sol
+++ smartContracts/src/interfaces/IMediSyncRecords.sol
@@ -1,8 +1,2 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-interface IMediSyncRecords {
-    function bioEntry(bytes memory _calldata, address _patient) external;
-    function retrieveRecordStatus(address _patientAddress)view external returns(bool);
-    function uploadSession(address _patient, uint sessionID, bytes _calldata) external;
-
-}
