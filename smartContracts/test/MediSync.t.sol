917c1b502f03cb33cd3aa560e1ae4d334fcca1c4
aitchIV <uchennavictor248@gmail.com>
2023-10-21 16:23:58 +0100

    added test file

---
+++ smartContracts/test/MediSync.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/MediSyncFactory.sol";
+import "../src/MediSyncHospital.sol";
+import "../src/MediSyncPrescriptio.sol";
+import "../src/MediSyncRecords.sol";
+
+contract MedisyncTest is Test {}
